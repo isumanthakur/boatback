9391be0c9642c208151deeef9013697ef2a40508
iSumanThakur436 <tsuman436@gmail.com>
2024-09-01 00:52:01 +0530

    Update CORS and CSRF serializer to allow Vercel frontend2

--- community/serializers.py
+++ community/serializers.py
@@ -1,27 +1,11 @@
 from rest_framework import serializers
 from .models import Post, Like, Comment
 
-class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Comment
-        fields = ['id', 'post', 'user', 'text', 'created_at']
-
-    def get_user(self, obj):
-        return {
-            "id": obj.user.id,
-            "name": obj.user.name,
-            "avatar_url": obj.user.avatar_url(),
-        }
-
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     likes_count = serializers.IntegerField(read_only=True)
     comments_count = serializers.IntegerField(read_only=True)
     comments = serializers.SerializerMethodField()
-    image = serializers.SerializerMethodField()
-    video = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
@@ -38,19 +22,22 @@ class PostSerializer(serializers.ModelSerializer):
         # Ensure comments is always an array
         return CommentSerializer(obj.comments.all(), many=True).data if obj.comments.exists() else []
 
-    def get_image(self, obj):
-        request = self.context.get('request')
-        if obj.image and request:
-            return request.build_absolute_uri(obj.image.url)
-        return None
-
-    def get_video(self, obj):
-        request = self.context.get('request')
-        if obj.video and request:
-            return request.build_absolute_uri(obj.video.url)
-        return None
-
+    
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'post', 'user', 'created_at']
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'user', 'text', 'created_at']
+
+    def get_user(self, obj):
+        return {
+            "id": obj.user.id,
+            "name": obj.user.name,
+            "avatar_url": obj.user.avatar_url(),
+        }
--- property/serializers.py
+++ property/serializers.py
@@ -2,10 +2,7 @@ from rest_framework import serializers
 from .models import Property, Reservation
 from useraccount.serializers import UserDetailSerializer
 
-# Serializer for the list view of properties
 class PropertiesListSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
-
     class Meta:
         model = Property
         fields = (
@@ -13,20 +10,12 @@ class PropertiesListSerializer(serializers.ModelSerializer):
             'title',
             'price_per_night',
             'image_url',
-            'state',
-            'city',
+            'state',  # New field
+            'city',   # New field
         )
 
-    def get_image_url(self, obj):
-        request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url)
-        return None
-
-# Serializer for the detailed view of a property
 class PropertiesDetailSerializer(serializers.ModelSerializer):
     landlord = UserDetailSerializer(read_only=True, many=False)
-    image_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Property
@@ -39,28 +28,16 @@ class PropertiesDetailSerializer(serializers.ModelSerializer):
             'bedrooms',
             'bathrooms',
             'guests',
-            'state',
-            'city',
+            'state',  # New field
+            'city',   # New field
             'landlord'
         )
 
-    def get_image_url(self, obj):
-        request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url)
-        return None
-
-# Serializer for the list view of reservations
 class ReservationsListSerializer(serializers.ModelSerializer):
     property = PropertiesListSerializer(read_only=True, many=False)
     
     class Meta:
         model = Reservation
         fields = (
-            'id', 
-            'start_date', 
-            'end_date', 
-            'number_of_nights', 
-            'total_price', 
-            'property'
+            'id', 'start_date', 'end_date', 'number_of_nights', 'total_price', 'property'
         )
